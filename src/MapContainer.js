375af5f7d6624c0ecd8a0a303be228b8f8830399
Stefanos Gkouveris <stefanos.gkouveris@gmail.com>
2018-08-02 19:13:26 +0100

    Add Wonder ICONS
    - Each Wonder of the World has a different icon displayed

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -5,9 +5,28 @@ export class MapContainer extends React.Component {
   constructor(props) {
     super(props)
       this.state = {
-            wonders: []
+          wonders: [],
+          showingInfoWindow: false,
+          activeMarker: {},
+          selectedPlace: {}
         }
   }
+
+  onMarkerClick = (props, marker, e) =>
+  this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+  });
+
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
 
   componentDidMount() {
     let data = require('./wotwdata.json');
@@ -21,128 +40,265 @@ export class MapContainer extends React.Component {
   }
 
   render() {
-  console.log(this.state.wonders)
+  const { wonders } = this.state
+  const { google } = this.props
+
+  console.log(wonders)
+
   let styles =  [
     {
-        "featureType": "all",
-        "elementType": "all",
-        "stylers": [
-            {
-                "saturation": "32"
-            },
-            {
-                "lightness": "-3"
-            },
-            {
-                "visibility": "on"
-            },
-            {
-                "weight": "1.18"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative",
         "elementType": "labels",
         "stylers": [
             {
                 "visibility": "off"
+            },
+            {
+                "color": "#f49f53"
             }
         ]
     },
     {
         "featureType": "landscape",
-        "elementType": "labels",
         "stylers": [
             {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape.man_made",
-        "elementType": "all",
-        "stylers": [
-            {
-                "saturation": "-70"
+                "color": "#f9ddc5"
             },
             {
-                "lightness": "14"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
+                "lightness": -7
             }
         ]
     },
     {
         "featureType": "road",
-        "elementType": "labels",
+        "stylers": [
+            {
+                "color": "#813033"
+            },
+            {
+                "lightness": 43
+            }
+        ]
+    },
+    {
+        "featureType": "poi.business",
+        "stylers": [
+            {
+                "color": "#645c20"
+            },
+            {
+                "lightness": 38
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "color": "#1994bf"
+            },
+            {
+                "saturation": -69
+            },
+            {
+                "gamma": 0.99
+            },
+            {
+                "lightness": 43
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#f19f53"
+            },
+            {
+                "weight": 1.3
+            },
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "poi.business"
+    },
+    {
+        "featureType": "poi.park",
+        "stylers": [
+            {
+                "color": "#645c20"
+            },
+            {
+                "lightness": 39
+            }
+        ]
+    },
+    {
+        "featureType": "poi.school",
+        "stylers": [
+            {
+                "color": "#a95521"
+            },
+            {
+                "lightness": 35
+            }
+        ]
+    },
+    {},
+    {
+        "featureType": "poi.medical",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#813033"
+            },
+            {
+                "lightness": 38
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {
+        "elementType": "labels"
+    },
+    {
+        "featureType": "poi.sports_complex",
+        "stylers": [
+            {
+                "color": "#9e5916"
+            },
+            {
+                "lightness": 32
+            }
+        ]
+    },
+    {},
+    {
+        "featureType": "poi.government",
+        "stylers": [
+            {
+                "color": "#9e5916"
+            },
+            {
+                "lightness": 46
+            }
+        ]
+    },
+    {
+        "featureType": "transit.station",
         "stylers": [
             {
                 "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.line",
+        "stylers": [
+            {
+                "color": "#813033"
+            },
+            {
+                "lightness": 22
             }
         ]
     },
     {
         "featureType": "transit",
-        "elementType": "labels",
         "stylers": [
             {
-                "visibility": "off"
+                "lightness": 38
             }
         ]
     },
     {
-        "featureType": "water",
-        "elementType": "all",
+        "featureType": "road.local",
+        "elementType": "geometry.stroke",
         "stylers": [
             {
-                "saturation": "100"
+                "color": "#f19f53"
             },
             {
-                "lightness": "-14"
+                "lightness": -10
             }
         ]
     },
-    {
-        "featureType": "water",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            },
-            {
-                "lightness": "12"
-            }
-        ]
-    }]
+    {},
+    {},
+    {}
+]
 
-     return (
-      <div>
-         <Map
-           styles={styles}
-           google={this.props.google}
-           zoom={3}
-           initialCenter={{
-            lat: 23.5065756,
-            lng: -27.7048251
-          }}>
+   return (
+    <div>
+       <Map
+         styles={styles}
+         google={this.props.google}
+         zoom={3}
+         onClick={this.onMapClicked}
+         initialCenter={{
+          lat: 23.5065756,
+          lng: -27.7048251
+        }}>
+        {
+          wonders.filter(wonder => wonder.age === 'New')
+          .map( (wonder) => (
           <Marker
-            title={'The marker`s title will appear as a tooltip.'}
-            name={'SOMA'}
-            position={{lat: 37.778519, lng: -122.405640}} />
+            onClick={this.onMarkerClick}
+            key = {wonder.location.lat}
+            title={wonder.name}
+            name={wonder.name}
+            animation={google.maps.Animation.DROP}
+            icon={{
+                url: wonder.image,
+                scaledSize: new google.maps.Size(65,65)
+              }}
+            position={
+              {lat: wonder.location.lat,
+               lng: wonder.location.lng}
+             } />)
+           )
+         }
+         {
+           wonders.filter(wonder => wonder.age === 'Ancient')
+           .map( (wonder) => (
+           <Marker
+             key={wonder.location.lat}
+             name={wonder.name}
+             animation={google.maps.Animation.DROP}
+             icon={{
+                 url: wonder.image,
+                 scaledSize: new google.maps.Size(50,50)
+               }}
+             position={
+               {lat: wonder.location.lat,
+                lng: wonder.location.lng}
+              } />)
+            )
+        }
+      </Map>
+    </div>
+   );
+ }
 
-        </Map>
-      </div>
-
-     );
-   }
 }
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCxyj_aE9VcVXbJ0iD9vOk8rTmYwpuGGkY'
